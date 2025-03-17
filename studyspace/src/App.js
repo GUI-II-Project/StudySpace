f146ad06e63b351963a1b45fc9192b150c38bdca
Sunny <123197224+Zanite9@users.noreply.github.com>
2025-03-16 22:08:02 -0400

    Updated navigation and web page
    Added interaction through the page.

--- studyspace/src/App.js
+++ studyspace/src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
 import LandingPage from './pages/Landing';
+import NotesPage from './pages/NotesPage';
+import Calendar from './pages/Calendar';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <LandingPage />
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/notes" element={<NotesPage />} />
+          <Route path="/calendar" element={<Calendar />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- studyspace/src/components/Document.js
+++ studyspace/src/components/Document.js
@@ -1,12 +1,10 @@
-import '../notes.css';
-import React from 'react';
+import "../notes.css";
+import React from "react";
 
 class Document extends React.Component {
-    render() {
-        return (
-            <div contentEditable="true" class="editor"></div>
-        );
-    }
+  render() {
+    return <div contentEditable="true" class="editor"></div>;
+  }
 }
 
 export default Document;
