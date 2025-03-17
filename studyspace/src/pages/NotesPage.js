9f821de5e69eab5f06a050b374e233af4d668e7e
haddock21 <nikolas_haddock@student.uml.edu>
2025-03-16 20:17:03 -0400

    initial notes commit

---
+++ studyspace/src/pages/NotesPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import NavBar from '../components/NavBar.js';
+import ToolBar from '../components/ToolBar.js';
+import Document from '../components/Document.js';
+
+function NotesPage() {
+    return (
+        <div>
+            <NavBar />
+            <ToolBar />
+            <Document />
+        </div>
+    );
+}
+
+export default NotesPage
