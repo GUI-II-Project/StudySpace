5b74a2e5aa3329a6446d9f9390c59856697d38d9
dlee <55562781+dlee1337@users.noreply.github.com>
2025-03-17 17:30:32 -0400

    Create Player.js

---
+++ studyspace/src/components/Player.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import ReactPlayer from "react-player";
+
+/* https://www.youtube.com/watch?v=VGSFKeGFWEA -> Youtube video for testing*/
+
+const Player = () => {
+  return (
+    <div>
+      <div className="video-wrapper">
+      </div>
+    </div>
+  );
+};
+
+export default Player;
