506efddfc7d7d035e96da418e633ecccb105d47c
haddock21 <nikolas_haddock@student.uml.edu>
2025-03-22 14:07:48 -0400

    <i> to <span>

--- studyspace/src/components/ToolBar.js
+++ studyspace/src/components/ToolBar.js
@@ -8,38 +8,38 @@ class ToolBar extends React.Component {
             <div className='format-bar d-flex align-items-center justify-content-center'>
                 <div className='format-buttons m-0 px-2 d-flex align-items-center justify-content-center'>
                     <button class='tool' type='button' data-bs-toggle='dropdown' aria-expanded='false' >
-                        <i class='bi bi-plus-circle'></i></button>
+                        <span class='bi bi-plus-circle'></span></button>
                     <ul className='dropdown-menu'>
                         <li><button className='dropdown-item'><u>New Note</u></button></li>
                     </ul>
-                    <button class='tool'><i class='bi bi-download'></i></button>
+                    <button class='tool'><span class='bi bi-download'></span></button>
                     <button class='tool' onClick={() => onFormat('undo')} onMouseDown={(e) => e.preventDefault()}>
-                        <i class='bi bi-arrow-counterclockwise'></i></button>
+                        <span class='bi bi-arrow-counterclockwise'></span></button>
                     <button class='tool' onClick={() => onFormat('redo')} onMouseDown={(e) => e.preventDefault()}>
-                        <i class='bi bi-arrow-clockwise'></i></button>
+                        <span class='bi bi-arrow-clockwise'></span></button>
                     <input type='text' placeholder='Times New...'></input>
                     <button className={`tool ${activeFormats.bold ? 'active' : ''}`} onClick={() => onFormat('bold')} onMouseDown={(e) => e.preventDefault()}>
-                        <i class='bi bi-type-bold'></i></button>
+                        <span class='bi bi-type-bold'></span></button>
                     <button className={`tool ${activeFormats.italic ? 'active' : ''}`} onClick={() => onFormat('italic')} onMouseDown={(e) => e.preventDefault()}>
-                        <i class='bi bi-type-italic'></i></button>
+                        <span class='bi bi-type-italic'></span></button>
                     <button className={`tool ${activeFormats.underline ? 'active' : ''}`} onClick={() => onFormat('underline')} onMouseDown={(e) => e.preventDefault()}>
-                        <i class='bi bi-type-underline'></i></button>
+                        <span class='bi bi-type-underline'></span></button>
                     <button class='tool'>A</button>
                     <div id='size'>
-                        <button class='tool'><i class='bi bi-dash'></i></button>
+                        <button class='tool'><span class='bi bi-dash'></span></button>
                         <input type='numeric' value='10' ></input>
-                        <button class='tool'><i class='bi bi-plus'></i></button>
+                        <button class='tool'><span class='bi bi-plus'></span></button>
                     </div>
                     <button class='tool' onClick={() => onFormat('justifyLeft')} onMouseDown={(e) => e.preventDefault()}>
-                        <i class='bi bi-text-left'></i></button>
+                        <span class='bi bi-text-left'></span></button>
                     <button class='tool' onClick={() => onFormat('justifyCenter')} onMouseDown={(e) => e.preventDefault()}>
-                        <i class='bi bi-text-center'></i></button>
+                        <span class='bi bi-text-center'></span></button>
                     <button class='tool' onClick={() => onFormat('justifyRight')} onMouseDown={(e) => e.preventDefault()}>
-                        <i class='bi bi-text-right'></i></button>
+                        <span class='bi bi-text-right'></span></button>
                     <button class='tool' onClick={() => onFormat('insertUnorderedList')} onMouseDown={(e) => e.preventDefault()}>
-                        <i class='bi bi-list-ul'></i></button>
+                        <span class='bi bi-list-ul'></span></button>
                     <button class='tool' onClick={() => onFormat('insertOrderedList')} onMouseDown={(e) => e.preventDefault()}>
-                        <i class='bi bi-list-ol'></i></button>
+                        <span class='bi bi-list-ol'></span></button>
                 </div>
             </div>
         );
