5ed011afbe94504f0c11bd97010998201929f29d
Sunny <123197224+Zanite9@users.noreply.github.com>
2025-04-09 18:02:06 -0400

    Linting

--- studyspace/src/components/ToolBar.js
+++ studyspace/src/components/ToolBar.js
@@ -25,7 +25,7 @@ class ToolBar extends React.Component {
     return (
       <div className="format-bar d-flex align-items-center justify-content-center">
         <div className="format-buttons m-0 px-2 d-flex align-items-center justify-content-center">
-         {/*  <button
+          {/*  <button
             className="tool"
             type="button"
             data-bs-toggle="dropdown"
